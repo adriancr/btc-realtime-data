ef2f514136a8ae67852350ff1af8647f13bf382c
Adrian Esquivel <adrian@kapiwebdev.com>
2021-07-06 19:22:51 -0600

    Unstable, testing get_ip()

--- binance-stream-server/bin_server.py
+++ binance-stream-server/bin_server.py
@@ -5,8 +5,27 @@ import json
 from bson import json_util
 from DAL.Tick import Tick
 
+import socket
+
+
+ip = False
+port = 3549
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 async def binance_stream_server(websocket, path):
     tick = Tick()
+    
     while True:
         data = tick.getLatest()
         event = False
@@ -34,15 +53,12 @@ async def binance_stream_server(websocket, path):
     # print(f"> {greeting}")
 
 try:
-    print('1')
-    start_server = websockets.serve(binance_stream_server, "192.168.224.3", 3549)
-
-
-    print('2')
+    ip = get_ip()
+    print(f'Beginning stream at ws://{ip}:{port}')
+    start_server = websockets.serve(binance_stream_server, ip, port)
     asyncio.get_event_loop().run_until_complete(start_server)
-
-
-    print('3')
+    
+    print('Connection completed, stream running')
     asyncio.get_event_loop().run_forever()
 
 
